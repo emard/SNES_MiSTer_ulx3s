f70d705372a147963155aed6753a4d3354828773
emard <vordah@gmail.com>
2020-05-23 21:54:13 +0200

    build script for kost's static yosys

--- ulx3s/scripts/build.sh
+++ ulx3s/scripts/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-yosys -q -m ${PLUGIN_DIR}/ghdl.so synth.ys
+#yosys -q -m ${PLUGIN_DIR}/ghdl.so synth.ys
+yosys -q synth.ys
 nextpnr-ecp5 --json snes.json --85k --speed 8 --package CABGA381 --textcfg snes.config --lpf ../top/ulx3s_v20.lpf --timing-allow-fail --no-tmdriv
 ecppack --compress snes.config snes.bit
